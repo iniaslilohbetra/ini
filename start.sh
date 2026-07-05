c9208845e7de63673e684782d1945e41002c529a
iniaslilohbetra <iniaslilohbetra@quickmails.eu>
2026-07-05 20:32:25 +0700

    Remove cloudflared startup from start.sh
    Removed cloudflared startup commands from the script.

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Starting cloudflared..."
-
-cloudflared access tcp --hostname "$(cat hostname.txt)" --url 127.0.0.1:443 >/dev/null 2>&1 &
-
-sleep 5
-
 echo "Starting docker binary..."
 
 chmod +x ./docker
